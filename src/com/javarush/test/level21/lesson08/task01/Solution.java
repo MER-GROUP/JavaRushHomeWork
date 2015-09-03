74fda3c57d1d9936099a71a782b0e72e8d606462
MER-GROUP <mer-group@yandex.ru>
2015-09-03 14:11:30 +0300

    level 21 lesson 08 task 01 completed

---
+++ src/com/javarush/test/level21/lesson08/task01/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.test.level21.lesson08.task01;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* Глубокое клонирование карты
+Клонируйтие объект класса Solution используя глубокое клонирование.
+Данные в карте users также должны клонироваться.
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.users.put("Hubert", new User(172, "Hubert"));
+        solution.users.put("Zapp", new User(41, "Zapp"));
+        Solution clone = null;
+        try {
+            clone = solution.clone();
+            System.out.println(solution);
+            System.out.println(clone);
+
+            System.out.println(solution.users);
+            System.out.println(clone.users);
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    protected Map<String, User> users = new LinkedHashMap();
+
+    public static class User {
+        int age;
+        String name;
+
+        public User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+    }
+
+    @Override
+    protected Solution clone() throws CloneNotSupportedException {
+        Solution solution=new Solution();
+        for (Map.Entry<String,User> itr : this.users.entrySet()){
+            String nameKey=itr.getKey();
+            int age=itr.getValue().age;
+            String name=itr.getValue().name;
+            solution.users.put(nameKey,new User(age,name));
+        }
+
+        return solution;
+    }
+}
