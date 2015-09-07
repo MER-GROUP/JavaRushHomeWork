f6339e9ce42b9847b85e83a11220a562dfc4c19a
MER-GROUP <mer-group@yandex.ru>
2015-09-07 15:28:39 +0300

    level 21 lesson 16 big 01 part 06 completed

--- src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
+++ src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
@@ -7,12 +7,23 @@ import java.util.ArrayList;
  */
 public class Hippodrome {
     private ArrayList<Horse> horses;
+    public static Hippodrome game;
 
     public ArrayList<Horse> getHorses() {
         return horses;
     }
 
     public static void main(String[] args) {
-        //
+        game=new Hippodrome();
+
+        Horse firstHorse=new Horse("firstHorse",3,0);
+        Horse secondHorse=new Horse("secondHorse",3,0);
+        Horse thirdHorse=new Horse("thirdHorse",3,0);
+
+        game.horses=new ArrayList<Horse>();
+
+        game.getHorses().add(firstHorse);
+        game.getHorses().add(secondHorse);
+        game.getHorses().add(thirdHorse);
     }
 }
