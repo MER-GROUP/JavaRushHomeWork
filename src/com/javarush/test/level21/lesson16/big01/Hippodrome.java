e2e3b74414d4bd17e49ce269e70cbdc6e66057e6
MER-GROUP <mer-group@yandex.ru>
2015-09-07 14:36:08 +0300

    level 21 lesson 16 big 01 part 03 completed

--- src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
+++ src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
@@ -1,9 +1,17 @@
 package com.javarush.test.level21.lesson16.big01;
 
+import java.util.ArrayList;
+
 /**
  * Created by maxim on 05.09.2015.
  */
 public class Hippodrome {
+    private ArrayList<Horse> horses;
+
+    public ArrayList<Horse> getHorses() {
+        return horses;
+    }
+
     public static void main(String[] args) {
         //
     }
