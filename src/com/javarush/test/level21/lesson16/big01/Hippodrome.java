19749318f2a74b2c088ad30eeffad19b6002dde0
MER-GROUP <mer-group@yandex.ru>
2015-09-07 14:32:00 +0300

    level 21 lesson 16 big 01 part 02 completed

--- src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
+++ src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
@@ -4,4 +4,7 @@ package com.javarush.test.level21.lesson16.big01;
  * Created by maxim on 05.09.2015.
  */
 public class Hippodrome {
+    public static void main(String[] args) {
+        //
+    }
 }
