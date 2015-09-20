a9e199ef67366b1d293554273a782e02b6c7577f
MER-GROUP <mer-group@yandex.ru>
2015-09-20 11:54:05 +0300

    level 22 lesson 18 big 01 task 05 completed

--- src/com/javarush/test/level22/lesson18/big01/Tetris.java
+++ src/com/javarush/test/level22/lesson18/big01/Tetris.java
@@ -7,8 +7,12 @@ public class Tetris {
     private Field field;
     private Figure figure;
 
+    public static Tetris game;
+
     public static void main(String[] args) {
-        //
+        Tetris tetris=new Tetris();
+        game=tetris;
+        game.run();
     }
 
     public Field getField() {
