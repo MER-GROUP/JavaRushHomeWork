e47b59d6cdc27d0910863c730a73ebd31c0887ad
MER-GROUP <mer-group@yandex.ru>
2015-09-20 13:03:56 +0300

    level 22 lesson 18 big 01 task 11 completed

---
+++ src/com/javarush/test/level22/lesson18/big01/FigureFactory.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level22.lesson18.big01;
+
+/**
+ * Created by maxim on 20.09.2015.
+ */
+public class FigureFactory {
+    public static Figure createRandomFigure(int x,int y){
+        return null;
+    }
+}
