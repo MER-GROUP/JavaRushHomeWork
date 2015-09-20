4028d4a42b654f9e21c162c4d1b0cedb88beaf81
MER-GROUP <mer-group@yandex.ru>
2015-09-20 11:46:00 +0300

    level 22 lesson 18 big 01 task 03 completed

--- src/com/javarush/test/level22/lesson18/big01/Tetris.java
+++ src/com/javarush/test/level22/lesson18/big01/Tetris.java
@@ -4,7 +4,18 @@ package com.javarush.test.level22.lesson18.big01;
  * Created by maxim on 20.09.2015.
  */
 public class Tetris {
+    private Field field;
+    private Figure figure;
+
     public static void main(String[] args) {
         //
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
     }
 }
