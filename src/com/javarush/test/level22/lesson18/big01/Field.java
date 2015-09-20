05c68238461e79ade9aff53f0f6e2412d06d8a82
MER-GROUP <mer-group@yandex.ru>
2015-09-20 12:02:45 +0300

    level 22 lesson 18 big 01 task 06 completed

--- src/com/javarush/test/level22/lesson18/big01/Field.java
+++ src/com/javarush/test/level22/lesson18/big01/Field.java
@@ -4,4 +4,19 @@ package com.javarush.test.level22.lesson18.big01;
  * Created by maxim on 20.09.2015.
  */
 public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public int getWidth() {
+        return width;
+    }
 }
