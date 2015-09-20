f14cb25d610eaf4e454264040606d8b8885a890d
MER-GROUP <mer-group@yandex.ru>
2015-09-20 12:18:55 +0300

    level 22 lesson 18 big 01 task 08 completed

--- src/com/javarush/test/level22/lesson18/big01/Figure.java
+++ src/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -4,4 +4,19 @@ package com.javarush.test.level22.lesson18.big01;
  * Created by maxim on 20.09.2015.
  */
 public class Figure {
+    private int x;
+    private int y;
+    private int[][] matrix;
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
