ea1c6cd834082d879c5ef7dd0d2f40bfbe7bc17e
MER-GROUP <mer-group@yandex.ru>
2015-09-22 13:18:00 +0300

    level 22 lesson 18 big 01 task 16 completed

--- src/com/javarush/test/level22/lesson18/big01/Figure.java
+++ src/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -27,27 +27,33 @@ public class Figure {
     }
 
     public void left(){
-        //
+        x--;
+        if (!isCurrentPositionAvailable()) x++;
     }
 
     public void right(){
-        //
+        x++;
+        if (!isCurrentPositionAvailable()) x--;
     }
 
     public void down(){
-        //
+        y++;
     }
 
     public void up(){
-        //
+        y--;
     }
 
     public void downMaximum(){
-        //
+        while (isCurrentPositionAvailable())
+        {
+            y++;
+        }
+        y--;
     }
 
     public boolean isCurrentPositionAvailable(){
-        return false;
+        return true;
     }
 
     public void landed(){
--- src/com/javarush/test/level22/lesson18/big01/Tetris.java
+++ src/com/javarush/test/level22/lesson18/big01/Tetris.java
@@ -7,6 +7,7 @@ import java.awt.event.KeyEvent;
  */
 public class Tetris
 {
+
     private Field field;                //Поле с клетками
     private Figure figure;              //Фигурка
 
@@ -82,26 +83,22 @@ public class Tetris
         System.out.println("Game Over");
     }
 
-    /**
-     * Один шаг игры
-     */
     public void step()
     {
         //опускам фигурку вниз
         figure.down();
 
-        //если разместить фигурку на текущем месте невозможно:
-            //поднимаем обратно
-            //приземляем
-            //если фигурка приземлилась на самом верху - игра окончена
-            //удаляем заполненные линии
-            //создаем новую фигурку
-        if (!figure.isCurrentPositionAvailable()){
-            figure.up();
-            figure.landed();
-            isGameOver=figure.getY()<=1;
-            field.removeFullLines();
-            setFigure(FigureFactory.createRandomFigure(field.getWidth() / 2, 0));
+        //если разместить фигурку на текущем месте невозможно
+        if (!figure.isCurrentPositionAvailable())
+        {
+            figure.up();                    //поднимаем обратно
+            figure.landed();                //приземляем
+
+            isGameOver = figure.getY() <= 1;//если фигурка приземлилась на самом верху - игра окончена
+
+            field.removeFullLines();        //удаляем заполненные линии
+
+            figure = FigureFactory.createRandomFigure(field.getWidth() / 2, 0); //создаем новую фигурку
         }
     }
 
