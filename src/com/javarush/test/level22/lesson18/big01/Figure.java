d7b950a4fb2f03f9beaaeedadbc9f412512813f6
MER-GROUP <mer-group@yandex.ru>
2015-09-22 13:46:55 +0300

    level 22 lesson 18 big 01 task 17 completed

--- src/com/javarush/test/level22/lesson18/big01/Figure.java
+++ src/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -1,66 +1,152 @@
 package com.javarush.test.level22.lesson18.big01;
 
+
 /**
- * Created by maxim on 20.09.2015.
+ * Класс Figure описывает фигурку тетриса
  */
-public class Figure {
+public class Figure
+{
+    //метрица которая определяет форму фигурки: 1 - клетка не пустая, 0 - пустая
+    private int[][] matrix;
+    //координаты
     private int x;
     private int y;
-    private int[][] matrix;
 
-    public Figure(int x, int y, int[][] matrix) {
-        this.matrix = matrix;
+    public Figure(int x, int y, int[][] matrix)
+    {
         this.x = x;
         this.y = y;
+        this.matrix = matrix;
     }
 
-    public int[][] getMatrix() {
-        return matrix;
-    }
-
-    public int getX() {
+    public int getX()
+    {
         return x;
     }
 
-    public int getY() {
+    public int getY()
+    {
         return y;
     }
 
-    public void left(){
+    public int[][] getMatrix()
+    {
+        return matrix;
+    }
+
+    /**
+     * Поворачаиваем фигурку.
+     * Для простоты - просто вокруг главной диагонали.
+     */
+    public void rotate()
+    {
+        int[][] matrix2 = new int[3][3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                matrix2[i][j] = matrix[j][i];
+            }
+        }
+
+        matrix = matrix2;
+    }
+
+    /**
+     * Двигаем фигурку влево.
+     * Проверяем не вылезла ли она за границу поля и/или не залезла ли на занятые клетки.
+     */
+    public void left()
+    {
         x--;
-        if (!isCurrentPositionAvailable()) x++;
+        if (!isCurrentPositionAvailable())
+            x++;
     }
 
-    public void right(){
+    /**
+     * Двигаем фигурку вправо.
+     * Проверяем не вылезла ли она за границу поля и/или не залезла ли на занятые клетки.
+     */
+    public void right()
+    {
         x++;
-        if (!isCurrentPositionAvailable()) x--;
+        if (!isCurrentPositionAvailable())
+            x--;
     }
 
-    public void down(){
-        y++;
-    }
-
-    public void up(){
+    /**
+     * Двигаем фигурку вверх.
+     * Используется, если фигурка залезла на занятые клетки.
+     */
+    public void up()
+    {
         y--;
     }
 
-    public void downMaximum(){
+    /**
+     * Двигаем фигурку вниз.
+     */
+    public void down()
+    {
+        y++;
+    }
+
+    /**
+     * Двигаем фигурку вниз до тех пор, пока не залезем на кого-нибудь.
+     */
+    public void downMaximum()
+    {
         while (isCurrentPositionAvailable())
         {
             y++;
         }
+
         y--;
     }
 
-    public boolean isCurrentPositionAvailable(){
+    /**
+     * Проверяем - может ли фигурка находится на текущей позици:
+     * а) не вылазиет ли она за границы поля
+     * б) не залазиет ли она на занятые клетки
+     */
+    public boolean isCurrentPositionAvailable()
+    {
+        Field field = Tetris.game.getField();
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (matrix[i][j] == 1)
+                {
+                    if (y + i >= field.getHeight())
+                        return false;
+
+                    Integer value = field.getValue(x + j, y + i);
+                    if (value == null || value == 1)
+                        return false;
+                }
+            }
+        }
+
         return true;
     }
 
-    public void landed(){
-        //
-    }
+    /**
+     * Приземляем фигурку - добавляем все ее непустые клетки к клеткам поля.
+     */
+    public void landed()
+    {
+        Field field = Tetris.game.getField();
 
-    public void rotate() {
-        //
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (matrix[i][j] == 1)
+                    field.setValue(x + j, y + i, 1);
+            }
+        }
     }
 }
