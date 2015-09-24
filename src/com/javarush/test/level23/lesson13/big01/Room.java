e592f334d65c50326abce82451189e60dd7ab2be
MER-GROUP <mer-group@yandex.ru>
2015-09-24 14:37:38 +0300

    level 23 lesson 13 big 01 task 09 completed

--- src/com/javarush/test/level23/lesson13/big01/Room.java
+++ src/com/javarush/test/level23/lesson13/big01/Room.java
@@ -4,6 +4,45 @@ package com.javarush.test.level23.lesson13.big01;
  * Created by maxim on 24.09.2015.
  */
 public class Room {
+    private int width;
+    private int height;
+    private Snake snake;
+    private Mouse mouse;
+
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public Room(int width, int height, Snake snake) {
+        this.height = height;
+        this.width = width;
+        this.snake = snake;
+    }
+
     public static void main(String[] args) {
         //
     }
