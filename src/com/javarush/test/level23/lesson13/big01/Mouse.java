362a5e266a27b491c7ce93faa2a85022f7853907
MER-GROUP <mer-group@yandex.ru>
2015-09-24 12:22:43 +0300

    level 23 lesson 13 big 01 task 04 completed

--- src/com/javarush/test/level23/lesson13/big01/Mouse.java
+++ src/com/javarush/test/level23/lesson13/big01/Mouse.java
@@ -4,4 +4,19 @@ package com.javarush.test.level23.lesson13.big01;
  * Created by maxim on 24.09.2015.
  */
 public class Mouse {
+    private int x;
+    private int y;
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
