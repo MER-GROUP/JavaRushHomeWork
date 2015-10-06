199feff6a55cca2ba234bdd38da1f4a7b7b83235
MER-GROUP <mer-group@yandex.ru>
2015-10-06 13:06:14 +0300

    level 24 lesson 02 home 01 completed

---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarker.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by maxim on 06.10.2015.
+ */
+public interface SelfInterfaceMarker {
+}
---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by maxim on 06.10.2015.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public SelfInterfaceMarkerImpl() {
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
