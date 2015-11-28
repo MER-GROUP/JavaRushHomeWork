8a6a5c03755ca808d7c8a5fa53300ffc06e58469
MER-GROUP <mer-group@yandex.ru>
2015-11-28 11:50:27 +0300

    level 24 lesson 14 big 01 part 02 completed

--- src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
+++ src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
@@ -4,4 +4,7 @@ package com.javarush.test.level24.lesson14.big01;
  * Created by M.E.R.lin on 28.11.2015.
  */
 public class Arcanoid {
+    public static void main(String[] args) {
+        //
+    }
 }
