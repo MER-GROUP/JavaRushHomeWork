0bf1d66df8ed7255ae5e34fe3a846232f3297a6f
MER-GROUP <mer-group@yandex.ru>
2015-11-28 16:43:58 +0300

    level 24 lesson 14 big 01 part 18 completed

--- src/com/javarush/test/level24/lesson14/big01/Stand.java
+++ src/com/javarush/test/level24/lesson14/big01/Stand.java
@@ -1,43 +1,72 @@
 package com.javarush.test.level24.lesson14.big01;
 
 /**
- * Created by M.E.R.lin on 28.11.2015.
+ *  Подставка, с помощью которой отражаем мячик.
  */
-public class Stand extends BaseObject {
-    private double speed;
-    private double direction;
-    public double getSpeed() {
+public class Stand extends BaseObject
+{
+    //картинка для отрисовки
+    private static int[][] matrix = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
+
+    //скорость
+    private double speed = 1;
+    //направление (-1 влево, +1 вправо)
+    private double direction = 0;
+
+    public Stand(double x, double y)
+    {
+        super(x,y,3);
+    }
+
+    /**
+     * Метод передвигает подставку в соответствии с текущим значением direction.
+     */
+    public void move()
+    {
+        double dx = speed * direction;
+        x = x + dx;
+
+        checkBorders(radius, Arcanoid.game.getWidth() - radius + 1, 1, Arcanoid.game.getHeight() + 1);
+    }
+
+    /**
+     * direction устанавливается равным -1
+     */
+    public void moveLeft()
+    {
+        direction = -1;
+    }
+
+    /**
+     * direction устанавливается равным +1
+     */
+    public void moveRight()
+    {
+        direction = 1;
+    }
+
+    public double getSpeed()
+    {
         return speed;
     }
-    public double getDirection() {
+
+    public double getDirection()
+    {
         return direction;
     }
 
-    public Stand(double x, double y) {
-        super(x, y, 3);
-        speed=1;
-        direction=0;
-    }
-
+    /**
+     * Отрисовываем себя на холсте
+     */
     @Override
-    public void draw(Canvas canvas) {
-        //
-    }
-
-    @Override
-    public void move() {
-        if (1==getDirection()){
-            setX(getX()+getSpeed());
-        }
-        if (-1==getDirection()){
-            setX(getX()-getSpeed());
-        }
-    }
-
-    public void moveLeft(){
-        direction=-1;
-    }
-    public void moveRight(){
-        direction=1;
+    public void draw(Canvas canvas)
+    {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'M');
     }
 }
