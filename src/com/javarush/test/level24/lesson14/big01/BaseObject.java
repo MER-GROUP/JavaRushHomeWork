8a49ce50220813d1c6e6bf121b6a7a7f3149f519
MER-GROUP <mer-group@yandex.ru>
2015-11-28 13:49:32 +0300

    level 24 lesson 14 big 01 part 08 completed

--- src/com/javarush/test/level24/lesson14/big01/BaseObject.java
+++ src/com/javarush/test/level24/lesson14/big01/BaseObject.java
@@ -3,7 +3,7 @@ package com.javarush.test.level24.lesson14.big01;
 /**
  * Created by M.E.R.lin on 28.11.2015.
  */
-public class BaseObject {
+public abstract class BaseObject {
     private double x;
     private double y;
     private double radius;
@@ -29,5 +29,21 @@ public class BaseObject {
     }
     public void setRadius(double radius) {
         this.radius = radius;
+    }
+
+    public abstract void draw();
+    public abstract void move();
+
+    public boolean isIntersec(BaseObject o){
+        double x1=getX();
+        double x2=o.getX();
+        double y1=getY();
+        double y2=o.getY();
+        double r1=getRadius();
+        double r2=o.getRadius();
+        double distance=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        double maxRadius=Math.max(r1,r2);
+        if (distance<maxRadius) return true;
+        return false;
     }
 }
