9045d1dee4bbac91c11e7c53114ab4f348503796
MER-GROUP <mer-group@yandex.ru>
2015-11-28 12:38:25 +0300

    level 24 lesson 14 big 01 part 07 completed

--- src/com/javarush/test/level24/lesson14/big01/Ball.java
+++ src/com/javarush/test/level24/lesson14/big01/Ball.java
@@ -4,4 +4,7 @@ package com.javarush.test.level24.lesson14.big01;
  * Created by M.E.R.lin on 28.11.2015.
  */
 public class Ball extends BaseObject{
+    public Ball(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
--- src/com/javarush/test/level24/lesson14/big01/Stand.java
+++ src/com/javarush/test/level24/lesson14/big01/Stand.java
@@ -4,4 +4,7 @@ package com.javarush.test.level24.lesson14.big01;
  * Created by M.E.R.lin on 28.11.2015.
  */
 public class Stand extends BaseObject {
+    public Stand(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
