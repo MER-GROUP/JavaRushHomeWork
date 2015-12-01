cdd7b266d39a8532c51a39b5401a3cca79bcfd3a
MER-GROUP <mer-group@yandex.ru>
2015-12-01 13:11:53 +0300

    level 25 lesson 02 task 02 completed

---
+++ src/com/javarush/test/level25/lesson02/task02/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level25.lesson02.task02;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+/* Машину на СТО не повезем!
+Инициализируйте поле wheels используя данные из loadWheelNamesFromDB.
+Обработайте некорректные данные.
+Подсказка: если что-то не то с колесами, то это не машина!
+Сигнатуры не менять.
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() {
+            //init wheels here
+            wheels=new LinkedList<Wheel>();
+            try{
+                for (int i = 0; i < loadWheelNamesFromDB().length; i++) {
+                    wheels.add(Wheel.valueOf(loadWheelNamesFromDB()[i]));
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+}
