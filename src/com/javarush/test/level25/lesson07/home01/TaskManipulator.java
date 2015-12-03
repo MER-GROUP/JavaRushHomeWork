ddc080ec5468dd5e5877b131c98665ebbd7c09a4
MER-GROUP <mer-group@yandex.ru>
2015-12-03 14:30:08 +0300

    level 25 lesson 07 home 01 completed

---
+++ src/com/javarush/test/level25/lesson07/home01/TaskManipulator.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level25.lesson07.home01;
+
+public class TaskManipulator implements Runnable,CustomThreadManipulator {
+    private Thread thread;
+    private String threadName;
+
+    public TaskManipulator() {
+    }
+
+    @Override
+    public void run() {
+        try{
+            while (!thread.isInterrupted()){
+                Thread.sleep(0);
+                System.out.println(threadName);
+                Thread.sleep(90);
+            }
+        }catch (Exception e){
+            //
+        }
+    }
+
+    @Override
+    public void start(String threadName) {
+        this.threadName=threadName;
+        thread=new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        thread.interrupt();
+    }
+}
