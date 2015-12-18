17a66223466f87d82bf5277b130aee4f02e62154
MER-GROUP <mer-group@yandex.ru>
2015-12-18 13:25:42 +0300

    Level 25 Lesson 16 Big 01 Part 04 Completed

--- src/com/javarush/test/level25/lesson16/big01/Space.java
+++ src/com/javarush/test/level25/lesson16/big01/Space.java
@@ -1,9 +1,49 @@
 package com.javarush.test.level25.lesson16.big01;
 
+import java.util.ArrayList;
+
 /**
  * Created by M.E.R.lin on 18.12.2015.
  */
 public class Space {
+    private int width;
+    private int height;
+    private SpaceShip ship;
+    private ArrayList<Ufo> ufos;
+    private ArrayList<Rocket> rockets;
+    private ArrayList<Bomb> bombs;
+
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public SpaceShip getShip() {
+        return ship;
+    }
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
+    }
+    public ArrayList<Ufo> getUfos() {
+        return ufos;
+    }
+    public ArrayList<Rocket> getRockets() {
+        return rockets;
+    }
+    public ArrayList<Bomb> getBombs() {
+        return bombs;
+    }
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.ship=new SpaceShip();
+        this.ufos=new ArrayList<>();
+        this.rockets=new ArrayList<>();
+        this.bombs=new ArrayList<>();
+    }
+
     //main
     public static void main(String[] args) {
         //
