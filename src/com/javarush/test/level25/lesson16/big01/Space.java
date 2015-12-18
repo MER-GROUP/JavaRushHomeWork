0eec085da221b54d51b8671e05e98067ea80876d
MER-GROUP <mer-group@yandex.ru>
2015-12-18 13:02:52 +0300

    Level 25 Lesson 16 Big 01 Part 02 Completed

--- src/com/javarush/test/level25/lesson16/big01/Space.java
+++ src/com/javarush/test/level25/lesson16/big01/Space.java
@@ -4,4 +4,8 @@ package com.javarush.test.level25.lesson16.big01;
  * Created by M.E.R.lin on 18.12.2015.
  */
 public class Space {
+    //main
+    public static void main(String[] args) {
+        //
+    }
 }
