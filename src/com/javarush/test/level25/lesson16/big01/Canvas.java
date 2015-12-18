a8f909ae0aa97d818754a04d2b77bb8cf577641c
MER-GROUP <mer-group@yandex.ru>
2015-12-18 15:02:33 +0300

    Level 25 Lesson 16 Big 01 Part 08 Completed

--- src/com/javarush/test/level25/lesson16/big01/Canvas.java
+++ src/com/javarush/test/level25/lesson16/big01/Canvas.java
@@ -4,4 +4,23 @@ package com.javarush.test.level25.lesson16.big01;
  * Created by M.E.R.lin on 18.12.2015.
  */
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[getWidth()][getHeight()];
+    }
 }
