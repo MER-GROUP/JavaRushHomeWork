aa19473e74ec2a68b0ed11892dfb4e377a5c2cc7
MER-GROUP <mer-group@yandex.ru>
2015-12-18 15:33:19 +0300

    Level 25 Lesson 16 Big 01 Part 09 Completed

--- src/com/javarush/test/level25/lesson16/big01/Canvas.java
+++ src/com/javarush/test/level25/lesson16/big01/Canvas.java
@@ -23,4 +23,23 @@ public class Canvas {
         this.height = height;
         this.matrix = new char[getWidth()][getHeight()];
     }
+
+    public void setPoint(double x, double y, char c){
+        if (!(x<0 || y<0 || y>matrix.length || x>matrix[0].length)){
+            int x1= (int) Math.round(x);
+            int y1= (int) Math.round(y);
+            this.matrix[y1][x1]=c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c){
+        int height = matrix.length;
+        int width = matrix[0].length;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] != 0)
+                    setPoint(x + j, y + i, c);
+            }
+        }
+    }
 }
