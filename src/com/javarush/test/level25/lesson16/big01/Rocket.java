65946097f5b77b948a46a4f5f60e6a26c3302520
MER-GROUP <mer-group@yandex.ru>
2015-12-18 16:01:40 +0300

    Level 25 Lesson 16 Big 01 Part 12 Completed

--- src/com/javarush/test/level25/lesson16/big01/Rocket.java
+++ src/com/javarush/test/level25/lesson16/big01/Rocket.java
@@ -4,7 +4,15 @@ package com.javarush.test.level25.lesson16.big01;
  * Created by M.E.R.lin on 18.12.2015.
  */
 public class Rocket extends BaseObject{
-    public Rocket(double x, double y, double radius) {
-        super(x, y, radius);
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x,y,'R');
+    }
+
+    public void move() {
+        y--;
     }
 }
